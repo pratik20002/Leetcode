d2604d8c29414bc07f6b1bf111ed30bba8e351de
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-08-13 13:24:56 +0530

    Time: 41 ms (77.59%) | Memory: 17.8 MB (31.68%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,8 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         cleanString = re.sub(r'[\W_]+', '', s)
-        print(cleanString.lower())
-        print( cleanString.lower()[::-1])
+        # print(cleanString.lower())
+        # print( cleanString.lower()[::-1])
         if(cleanString.lower() == cleanString.lower()[::-1]):
             return True
         else:
