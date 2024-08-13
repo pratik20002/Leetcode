72325a327c6c5038813540455c3fac97122ee274
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-08-13 13:33:16 +0530

    Time: 53 ms (19.57%) | Memory: 21.8 MB (7.40%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,9 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        cleanString = re.sub(r'[\W_]+', '', s)
-        # print(cleanString.lower())
-        # print( cleanString.lower()[::-1])
-        if(cleanString.lower() == cleanString.lower()[::-1]):
-            return True
-        else:
-            return False
+        #cleanString = re.sub(r'[\W_]+', '', s)
+
+        # if(cleanString.lower() == cleanString.lower()[::-1]):
+        #     return True
+        # else:
+        #     return False
+
+        cleaned_string = ''.join(char.lower() for char in s if char.isalnum())
+        left, right = 0, len(cleaned_string) - 1
+
+        while left < right:
+            if cleaned_string[left] != cleaned_string[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
+            
