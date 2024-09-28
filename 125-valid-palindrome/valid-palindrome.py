2992472540988d091ef42659b740ef2671864502
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-09-28 23:03:16 +0530

    Time: 49 ms (34.16%) | Memory: 17 MB (83.88%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -7,15 +7,35 @@ class Solution:
         # else:
         #     return False
 
-        cleaned_string = ''.join(char.lower() for char in s if char.isalnum())
-        left, right = 0, len(cleaned_string) - 1
+        # cleaned_string = ''.join(char.lower() for char in s if char.isalnum())
+        # left, right = 0, len(cleaned_string) - 1
+
+        # while left < right:
+        #     if cleaned_string[left] != cleaned_string[right]:
+        #         return False
+        #     left += 1
+        #     right -= 1
+
+        # return True
+
+        left, right = 0, len(s) - 1
 
         while left < right:
-            if cleaned_string[left] != cleaned_string[right]:
-                return False
-            left += 1
-            right -= 1
+            while left < right and not self.isalphaNum(s[left]):
+                left += 1
+            while right > left and not self.isalphaNum(s[right]):
+                right -= 1
 
+            if s[left].lower() != s[right].lower():
+                return False
+            left, right = left + 1, right - 1
+        
         return True
+
+    
+    def isalphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
 
             
