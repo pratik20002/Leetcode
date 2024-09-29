c0a0353b8d35cdba2b333b1f2568df02dc8fb7fe
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-09-29 15:39:49 -0700

    Time: 65 ms (96.00%) | Memory: 17.1 MB (24.58%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -4,7 +4,9 @@ class Solution:
 
         while left < right:
             if s[left] != s[right]:
-                return (s[left + 1: right + 1] == s[left + 1: right + 1][::-1] or s[left:right] == s[left:right][::-1])
+                skipL, skipR = s[left + 1: right + 1], s[left:right]
+
+                return (skipL == skipL[::-1] or skipR == skipR[::-1])
             
             left, right = left + 1, right - 1
         
