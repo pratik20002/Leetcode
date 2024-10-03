e286550aa469806afbbb4be6f1758d3e7821975a
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-03 11:40:02 -0700

    Time: 34 ms (88.00%) | Memory: 16.5 MB (90.44%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,24 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        last = m + n - 1
-        
-        #merge in reverse order
+        insert_pos = m + n - 1
+
         while m > 0 and n > 0:
             if nums1[m - 1] > nums2[n - 1]:
-                nums1[last] = nums1[m - 1]
+                nums1[insert_pos] = nums1[m - 1]
                 m -= 1
+
             else:
-                nums1[last] = nums2[n - 1]
+                nums1[insert_pos] = nums2[n - 1]
                 n -= 1
-            last -= 1
+            
+            insert_pos -= 1
 
-        #fill nums1 with leftover in nums2
         while n > 0:
-            nums1[last] = nums2[n - 1]
+            nums1[insert_pos] = nums2[n - 1]
             n -= 1
-            last -= 1
-
-
-
+            insert_pos -= 1
         
