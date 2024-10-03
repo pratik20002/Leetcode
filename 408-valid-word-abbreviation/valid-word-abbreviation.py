6fe18a359df503edc0d1eec9e19239b5774faa42
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-03 11:05:58 -0700

    Time: 31 ms (83.45%) | Memory: 16.6 MB (13.95%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,7 +1,8 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        word_ptr = abbr_ptr = 0
-        while word_ptr < len(word) and abbr_ptr < len(abbr):
+        wrd_ptr = abbr_ptr = 0
+
+        while wrd_ptr < len(word) and abbr_ptr < len(abbr):
             if abbr[abbr_ptr].isdigit():
                 steps = 0
 
@@ -12,15 +13,13 @@ class Solution:
                     steps = steps * 10 + int(abbr[abbr_ptr])
                     abbr_ptr += 1
                 
-                word_ptr += steps
+                wrd_ptr += steps
             
             else:
-                if word[word_ptr] != abbr[abbr_ptr]:
+                if word[wrd_ptr] != abbr[abbr_ptr]:
                     return False
-
-                word_ptr += 1
+                
+                wrd_ptr += 1
                 abbr_ptr += 1
             
-        return word_ptr == len(word) and abbr_ptr == len(abbr)
-                
-                
+        return wrd_ptr == len(word) and abbr_ptr == len(abbr)
