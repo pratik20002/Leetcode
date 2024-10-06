c6780321ae7ec171868b5b1df43ff36b635555b6
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-05 19:24:18 -0700

    Time: 1274 ms (76.79%) | Memory: 21 MB (31.81%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
@@ -7,10 +7,10 @@ class SparseVector:
 
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
-        
-        #Hashmap and 2Pointer 
+
         dot_product = 0
         i = j = 0
+        
         while i < len(self.nums) and j < len(vec.nums):
             i_idx, i_num = self.nums[i]
             j_idx, j_num = vec.nums[j]
@@ -22,12 +22,12 @@ class SparseVector:
 
             elif i_idx > j_idx:
                 j += 1
-            
+
             else:
                 i += 1
-            
-        return dot_product
 
+        return dot_product        
+        
 # Your SparseVector object will be instantiated and called as such:
 # v1 = SparseVector(nums1)
 # v2 = SparseVector(nums2)
