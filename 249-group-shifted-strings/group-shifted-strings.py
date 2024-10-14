ac5ef2c001b58b5643fcd6caf486873a448ac742
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-13 18:49:00 -0700

    Time: 40 ms (51.66%) | Memory: 16.6 MB (64.32%) - LeetSync

--- 249-group-shifted-strings/group-shifted-strings.py
+++ 249-group-shifted-strings/group-shifted-strings.py
@@ -1,7 +1,20 @@
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
-        mp = defaultdict(list)
-        for s in strings:
-            key = tuple((ord(c)-ord(s[0]))%26 for c in s)
-            mp[key].append(s)
-        return mp.values()
+        grouping_dict = defaultdict(list)
+
+        for string in strings:
+            if len(string) == 1:
+                grouping_dict[(-1)].append(string)
+            
+            else:
+                char_diff = []
+                i = 1
+                while i < len(string):
+                    char_diff.append(
+                        (ord(string[i]) - ord(string[i-1])) % 26
+                    )
+                    i += 1
+                
+                grouping_dict[tuple(char_diff)].append(string)
+
+        return grouping_dict.values()
