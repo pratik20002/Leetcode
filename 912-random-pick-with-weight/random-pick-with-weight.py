2ee35736d30b47db87b87b865e04bd337bd63f53
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-17 07:28:00 -0700

    Time: 96 ms (99.99%) | Memory: 21.8 MB (5.62%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -1,25 +1,54 @@
 class Solution:
 
-    def __init__(self, w: List[int]):
-        self.prefix_sum = []
-        total = 0
-        for num in w:
-            total += num
-            self.prefix_sum.append(total)
+    # def __init__(self, w: List[int]):
+    #     self.prefix_sum = []
+    #     total = 0
+    #     for num in w:
+    #         total += num
+    #         self.prefix_sum.append(total)
 
+    #     self.total = total
+    # def pickIndex(self) -> int:
+    #     target = random.uniform(0, self.total)
+    #     l = 0
+    #     r = len(self.prefix_sum)
+
+    #     while l < r:
+    #         mid = (l + r) // 2
+    #         if self.prefix_sum[mid] < target:
+    #             l = mid + 1
+    #         else:
+    #             r = mid
+            
+    #     return l
+
+
+
+    def __init__(self, w: List[int]):
+        self.prefix_sums = []
+        total = 0
+
+        for weight in w:
+            total += weight
+
+            self.prefix_sums.append(total)
+        
         self.total = total
+    
     def pickIndex(self) -> int:
         target = random.uniform(0, self.total)
+        
         l = 0
-        r = len(self.prefix_sum)
+        r = len(self.prefix_sums)
 
         while l < r:
             mid = (l + r) // 2
-            if self.prefix_sum[mid] < target:
+            if self.prefix_sums[mid] < target:
                 l = mid + 1
+            
             else:
                 r = mid
-            
+
         return l
 
 # Your Solution object will be instantiated and called as such:
