cc87735950d1dd128cccbcf30920e4474992721f
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-21 21:08:09 -0700

    Time: 347 ms (89.73%) | Memory: 16.7 MB (16.64%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -7,9 +7,8 @@ class Solution:
         for i in range(n):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-
+            
             lo, hi = i + 1, n - 1
-
             while lo < hi:
                 cur_sum = nums[i] + nums[lo] + nums[hi]
 
@@ -18,9 +17,12 @@ class Solution:
 
                 if cur_sum == target:
                     return cur_sum
+                
                 elif cur_sum < target:
                     lo += 1
+                
                 else:
                     hi -= 1
-        
+
         return closest_sum
+                
