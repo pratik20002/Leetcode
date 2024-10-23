0a61e4503a5eb2c8365462a670dd9cf9e21ae3bb
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-22 22:49:23 -0700

    Time: 0 ms (100.00%) | Memory: 16.6 MB (27.44%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -3,13 +3,24 @@ class Solution:
         #base if x == 0  return 0
         #base2 if n == 0 return 1
 
+        # def helper(x, n):
+        #     if x == 0: return 0
+        #     if n == 0: return 1
+
+        #     res = helper(x, n // 2)
+        #     res = res * res
+        #     return x * res if n % 2 else res
+        
+        # res = helper(x, abs(n))
+        # return res if n >= 0 else 1/res
+
         def helper(x, n):
             if x == 0: return 0
             if n == 0: return 1
 
-            res = helper(x, n // 2)
-            res = res * res
-            return x * res if n % 2 else res
+            res = helper(x, n//2)
+            res = res * res        
+            return x * res if n%2 else res
         
         res = helper(x, abs(n))
-        return res if n >= 0 else 1/res        
+        return res if n >= 0 else 1/res
