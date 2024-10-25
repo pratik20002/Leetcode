bf3227ce69954cdcbe70a0d6055a203ed4f74e80
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-24 21:27:59 -0700

    Time: 0 ms (100.00%) | Memory: 16.7 MB (9.74%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,7 +1,24 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        total = ""
-        for num in digits:
-            total += str(num)
-        total = int(total) + 1
-        return [int(digit) for digit in str(total)]
+        # total = ""
+        # for num in digits:
+        #     total += str(num)
+        # total = int(total) + 1
+        # return [int(digit) for digit in str(total)]
+        
+        digits = digits[::-1]
+        one, i = 1, 0
+
+        while one:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    one = 0
+            else:
+                digits.append(1)
+                one = 0
+            i += 1
+        
+        return digits[::-1]
