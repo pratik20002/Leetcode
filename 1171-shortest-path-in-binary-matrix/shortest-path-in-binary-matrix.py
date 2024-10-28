1f8c611a923f4c311ef474f9c6f30cc7866fa44d
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-28 06:56:51 -0700

    Time: 147 ms (79.84%) | Memory: 17.1 MB (67.94%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,27 +1,6 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        # N = len(grid)
-        # queue = deque([(0, 0, 1)]) #r, c, Length of Step
-        # visit = set((0, 0))
-        # directions = [[0, 1], [1, 0], [0, -1], [-1, 0],
-        #               [1, 1], [-1, -1], [1, -1], [-1, 1]]
-        # while queue:
-
-        #     r, c, length = queue.popleft()
-        #     if min(r, c) < 0  or max(r, c) == N or grid[r][c]:
-        #         continue
-            
-        #     if r == N - 1 and c == N - 1:
-        #         return length
-            
-        #     for dr, dc in directions:
-        #         if(r + dr, c + dc) not in visit:
-        #             queue.append((r + dr, c + dc, length + 1))
-        #             visit.add((r + dr, c + dc))
-
-        # return -1
-
-        #approach 2
+       
         if grid[0][0] or grid[-1][-1]:
             return -1
 
@@ -44,6 +23,30 @@ class Solution:
                     queue.append((new_x, new_y, path_len + 1))
                 
         return -1
+
+
+         # N = len(grid)
+        # queue = deque([(0, 0, 1)]) #r, c, Length of Step
+        # visit = set((0, 0))
+        # directions = [[0, 1], [1, 0], [0, -1], [-1, 0],
+        #               [1, 1], [-1, -1], [1, -1], [-1, 1]]
+        # while queue:
+
+        #     r, c, length = queue.popleft()
+        #     if min(r, c) < 0  or max(r, c) == N or grid[r][c]:
+        #         continue
+            
+        #     if r == N - 1 and c == N - 1:
+        #         return length
+            
+        #     for dr, dc in directions:
+        #         if(r + dr, c + dc) not in visit:
+        #             queue.append((r + dr, c + dc, length + 1))
+        #             visit.add((r + dr, c + dc))
+
+        # return -1
+
+        #approach 2
         
 
         
