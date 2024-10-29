30974a770530a78ad45a488c51ae03e7b0f4d409
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-28 20:53:17 -0700

    Time: 0 ms (100.00%) | Memory: 16.9 MB (49.00%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        level_items = collections.defaultdict(list)
+        queue = collections.deque([(root, 0, 0)])
+
+        min_col = float("inf")
+        max_col = float("-inf")
+
+        while queue:
+            node, row, col = queue.popleft()
+
+            if col < min_col:
+                min_col = col
+            
+            if col > max_col:
+                max_col = col
+            
+            level_items[col].append((node.val, row))
+
+            if node.left:
+                queue.append((node.left, row + 1, col - 1))
+            
+            if node.right:
+                queue.append((node.right, row + 1, col + 1))
+            
+        res = []
+
+        for level in range(min_col, max_col + 1):
+            items = level_items[level]
+            items.sort(key = lambda x: (x[1], x[0]))
+            items = [val for val, _ in items]
+            res.append(items)
+        
+        return res
