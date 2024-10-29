c9290644c52c054b995ba1de60086811e61a6fcd
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-28 18:17:01 -0700

    Time: 76 ms (57.45%) | Memory: 27.7 MB (92.98%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -21,9 +21,9 @@ class Solution:
         for num in nums:
             if len(heap) < k:
                 heapq.heappush(heap, num)
-            else:
-                if num > heap[0]:
-                    heapq.heappop(heap)
-                    heapq.heappush(heap, num)
-
-        return heap[0]        
+            
+            elif num > heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, num)
+        
+        return heap[0]
