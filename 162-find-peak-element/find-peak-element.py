3a887691d0e1369fa24c6237db40c7170c17fd43
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-28 18:53:21 -0700

    Time: 0 ms (100.00%) | Memory: 16.7 MB (73.70%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,34 +1,16 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        # left, right = 0, len(nums)
-
-        # while left <= right:
-        #     mid = (left + right) // 2
-        #     mid_val = nums[mid]
-        #     to_left = nums[mid - 1] if mid > 0 else float("-inf")
-        #     to_right = nums[mid + 1] if mid < len(nums) - 1 else float("-inf")
-
-        #     if to_left < mid_val > to_right:
-        #         return mid
-            
-        #     elif to_left < mid_val < to_right:
-        #         left = mid + 1
-            
-        #     else:
-        #         right = mid - 1
-
-
         left, right = 0, len(nums)
 
         while left <= right:
-            mid = (left + right) // 2
-            
-            mid_val = nums[mid]
+            mid = (left + right)//2
 
+            mid_val = nums[mid]
+            
             to_left = nums[mid - 1] if mid > 0 else float("-inf")
             to_right = nums[mid + 1] if mid < len(nums) - 1 else float("-inf")
 
-            if to_left < mid_val > to_right:
+            if  to_left < mid_val > to_right:
                 return mid
             
             elif to_left < mid_val < to_right:
@@ -36,3 +18,5 @@ class Solution:
             
             else:
                 right = mid - 1
+
+        
