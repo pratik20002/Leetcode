a6506ae18bb18ff4cd0cbfdec56679724e7633b7
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-30 19:35:04 -0700

    Time: 7 ms (95.45%) | Memory: 29.9 MB (96.23%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -1,10 +1,10 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
         out = []
-        maxHeight = 0
-        for i in range((len(heights) - 1), -1, -1):
-            if heights[i] > maxHeight:
+        max_height = -1
+        for i in range(len(heights) - 1, -1, -1):
+            if heights[i] > max_height:
                 out.append(i)
-                maxHeight = heights[i]
-
+                max_height = heights[i]
+        
         return out[::-1]
