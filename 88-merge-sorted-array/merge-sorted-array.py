d7a64b317013dfb291ad8521171d08b99a472ed4
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-31 20:55:58 -0700

    Time: 0 ms (100.00%) | Memory: 16.8 MB (9.01%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -8,14 +8,12 @@ class Solution:
             if nums1[m - 1] > nums2[n - 1]:
                 nums1[insert_pos] = nums1[m - 1]
                 m -= 1
-
             else:
                 nums1[insert_pos] = nums2[n - 1]
                 n -= 1
-            
             insert_pos -= 1
         
         while n > 0:
             nums1[insert_pos] = nums2[n - 1]
-            insert_pos -= 1
             n -= 1
+            insert_pos -= 1
