aca20595e09722c8b29e15641e654bc88c1ffa3d
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-31 20:01:58 -0700

    Time: 0 ms (100.00%) | Memory: 16.6 MB (30.46%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -39,27 +39,19 @@ class Solution:
 
 
 
-
-
-
-
-
         if not root:
             return []
-
+        
         column_items = collections.defaultdict(list)
         queue = collections.deque([(0, root)])
 
         min_x = float("inf")
         max_x = float("-inf")
 
-        res = []
-
         while queue:
-
             x, node = queue.popleft()
             column_items[x].append(node.val)
-            
+
             min_x = min(min_x, x)
             max_x = max(max_x, x)
 
@@ -68,9 +60,16 @@ class Solution:
             
             if node.right:
                 queue.append((x + 1, node.right))
-
+        
+        res = []
         for level in range(min_x, max_x + 1):
             res.append(column_items[level])
         
         return res
+         
+        
 
+
+
+
+        
