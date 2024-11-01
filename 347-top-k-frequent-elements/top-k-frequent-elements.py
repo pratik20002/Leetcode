695a601a4419d241a2acc8428b2ae7825785a7ea
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-11-01 12:26:45 -0700

    Time: 7 ms (54.58%) | Memory: 23 MB (6.10%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -20,28 +20,20 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
        
-        # count = {}
-        # freq = [[] for i in range(len(nums) + 1)]
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
 
-        # for n in nums:
-        #     count[n] = 1 + count.get(n, 0)
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
         
-        # for n, c in count.items():
-        #     freq[c].append(n)
+        for n, c in count.items():
+            freq[c].append(n)
         
-        # res = []
+        res = []
 
-        # for i in range(len(freq) - 1, 0, -1):
-        #     for n in freq[i]:
-        #         res.append(n)
-        #         if len(res) == k:
-        #             return res
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
         
-        count = Counter(nums)
-        heap = []
-
-        for num, freq in count.items():
-            heapq.heappush(heap, (freq, num))
-            if len(heap) > k:
-                heapq.heappop(heap)
-        return [num for freq, num in heap]
