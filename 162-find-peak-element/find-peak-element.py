de51938aef2d0606a4fa3b27c95297024dbe0ad8
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-10-31 21:02:46 -0700

    Time: 0 ms (100.00%) | Memory: 16.6 MB (74.41%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -4,19 +4,16 @@ class Solution:
 
         while left <= right:
             mid = (left + right)//2
-
             mid_val = nums[mid]
-            
+
             to_left = nums[mid - 1] if mid > 0 else float("-inf")
             to_right = nums[mid + 1] if mid < len(nums) - 1 else float("-inf")
 
             if to_left < mid_val > to_right:
                 return mid
-            
             elif to_left < mid_val < to_right:
                 left = mid + 1
-            
             else:
                 right = mid - 1
-
+        
         
