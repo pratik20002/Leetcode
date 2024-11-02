714f11696020d1d9de6681a6988bec3d4a94099a
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-11-02 10:22:29 -0700

    Time: 186 ms (60.48%) | Memory: 18.4 MB (20.60%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,52 +1,28 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-       
-        if grid[0][0] or grid[-1][-1]:
+        n = len(grid)
+        if grid[0][0] or grid[n-1][n-1]:
             return -1
-
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
         
+        directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
+                     (0, 1), (1, -1), (1, 0), (1, 1)]
+                    
         queue = collections.deque([(0, 0, 1)])
-        grid[0][0] = 1
+
+        visited = set((0, 0))
 
         while queue:
-            x, y, path_len = queue.popleft()
-            if (x, y) == (len(grid) - 1, len(grid[0]) - 1):
-                return path_len
+            r, c, pathlen = queue.popleft()
+            if r == (n - 1) and c == (n - 1):
+                return pathlen
             
-            for x_inc, y_inc in directions:
-                new_x = x + x_inc
-                new_y = y + y_inc
+            for dr, dc in directions:
+                n_row = r + dr
+                n_col = c + dc
 
-                if((0 <= new_x < len(grid)) and (0 <= new_y < len(grid[0])) and not grid[new_x][new_y]):
-                    grid[new_x][new_y] = 1
-                    queue.append((new_x, new_y, path_len + 1))
-                
+                if 0 <= n_row < n and 0 <= n_col < n and (n_row, n_col) not in visited and grid[n_row][n_col] == 0:
+                    visited.add((n_row, n_col))
+                    queue.append((n_row, n_col, pathlen + 1))
+            
         return -1
-
-
-         # N = len(grid)
-        # queue = deque([(0, 0, 1)]) #r, c, Length of Step
-        # visit = set((0, 0))
-        # directions = [[0, 1], [1, 0], [0, -1], [-1, 0],
-        #               [1, 1], [-1, -1], [1, -1], [-1, 1]]
-        # while queue:
-
-        #     r, c, length = queue.popleft()
-        #     if min(r, c) < 0  or max(r, c) == N or grid[r][c]:
-        #         continue
-            
-        #     if r == N - 1 and c == N - 1:
-        #         return length
-            
-        #     for dr, dc in directions:
-        #         if(r + dr, c + dc) not in visit:
-        #             queue.append((r + dr, c + dc, length + 1))
-        #             visit.add((r + dr, c + dc))
-
-        # return -1
-
-        #approach 2
-        
-
-        
+                
