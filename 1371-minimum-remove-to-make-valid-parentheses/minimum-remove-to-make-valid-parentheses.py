e63e26d388c1364fe0009ddf76793e0fc3a98444
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-11-02 08:42:58 -0700

    Time: 35 ms (76.07%) | Memory: 19.4 MB (15.88%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -2,26 +2,21 @@ class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         res = []
         cnt = 0
-
         for char in s:
             if char == "(":
                 res.append(char)
                 cnt += 1
-            
             elif char == ")" and cnt > 0:
                 res.append(char)
                 cnt -= 1
-            
             elif char != ")":
                 res.append(char)
-
+            
         filtered_str = []
-
-        for c in res[::-1]:
-            if c == "(" and cnt > 0:
+        for char in res[::-1]:
+            if char == "(" and cnt > 0:
                 cnt -= 1
             else:
-                filtered_str.append(c)
+                filtered_str.append(char)
         
         return "".join(filtered_str[::-1])
-                
