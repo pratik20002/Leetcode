1dd8b9cacd5b3544eee2ced9bde72b74398e9b66
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-11-02 11:15:10 -0700

    Time: 0 ms (100.00%) | Memory: 16.6 MB (66.50%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -35,41 +35,26 @@ class Solution:
         
         # return res
 
-
-
-
-
         if not root:
             return []
         
         column_items = collections.defaultdict(list)
-        queue = collections.deque([(0, root)])
-
-        min_x = float("inf")
-        max_x = float("-inf")
+        queue = collections.deque([(root, 0)])
 
         while queue:
-            x, node = queue.popleft()
-            column_items[x].append(node.val)
-
-            min_x = min(min_x, x)
-            max_x = max(max_x, x)
+            node, col = queue.popleft()
+            column_items[col].append(node.val)
 
             if node.left:
-                queue.append((x - 1, node.left))
+                queue.append((node.left, col - 1))
             
             if node.right:
-                queue.append((x + 1, node.right))
-        
+                queue.append((node.right, col + 1))
+            
         res = []
-        for level in range(min_x, max_x + 1):
-            res.append(column_items[level])
+
+        for column in sorted(column_items.keys()):
+            res.append(column_items[column])
         
         return res
-         
-        
-
-
-
-
         
