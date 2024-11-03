21a4e7a5bcdf53e6a15e8af545ec71ee57aaf018
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-11-02 19:47:48 -0700

    Time: 11 ms (37.92%) | Memory: 16.9 MB (92.32%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,12 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        left, right = 0, len(s) - 1
-
+        left , right = 0, len(s) - 1
         while left < right:
-            while left < right and not self.isalpha(s[left]):
+            while left < right and not s[left].isalnum():
                 left += 1
             
-            while left < right and not self.isalpha(s[right]):
+            while left < right and not s[right].isalnum():
                 right -= 1
             
             if s[left].lower() != s[right].lower():
@@ -16,9 +15,3 @@ class Solution:
             right -= 1
         
         return True
-    
-
-    def isalpha(self, c):
-        return (ord("A") <= ord(c) <= ord("Z") or
-                ord("a") <= ord(c) <= ord("z") or
-                ord("0") <= ord(c) <= ord("9"))
