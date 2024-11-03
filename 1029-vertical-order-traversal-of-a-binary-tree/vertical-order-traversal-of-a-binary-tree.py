144750a5279e5891f36bec77fa3b87226d59afc3
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-11-02 22:13:10 -0700

    Time: 0 ms (100.00%) | Memory: 17.1 MB (8.39%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -6,18 +6,17 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
         # if not root:
         #     return []
 
-        # level_items = collections.defaultdict(list)
+        # level_items =  collections.defaultdict(list)
         # queue = collections.deque([(root, 0, 0)])
-
         # min_col = float("inf")
         # max_col = float("-inf")
 
         # while queue:
         #     node, row, col = queue.popleft()
-
         #     if col < min_col:
         #         min_col = col
             
@@ -35,43 +34,29 @@ class Solution:
         # res = []
         # for level in range(min_col, max_col + 1):
         #     items = level_items[level]
-        #     items.sort(key = lambda x: (x[1], x[0]))
+        #     items.sort(key = lambda x:(x[1], x[0]))
         #     items = [val for val, _ in items]
         #     res.append(items)
         
         # return res
-    
-#T - > NlogN
-#S 
         if not root:
             return []
-
-        level_items =  collections.defaultdict(list)
+        
+        column_items = collections.defaultdict(list)
         queue = collections.deque([(root, 0, 0)])
-        min_col = float("inf")
-        max_col = float("-inf")
 
         while queue:
-            node, row, col = queue.popleft()
-            if col < min_col:
-                min_col = col
-            
-            if col > max_col:
-                max_col = col
-            
-            level_items[col].append((node.val, row))
-
-            if node.left:
-                queue.append((node.left, row + 1, col - 1))
-            
-            if node.right:
-                queue.append((node.right, row + 1, col + 1))
+            node, col, row = queue.popleft()
+            if node:
+                column_items[col].append((row, node.val))
+                queue.append((node.left, col - 1, row + 1))
+                queue.append((node.right, col + 1, row + 1))
         
+        sorted_columns = sorted(column_items.keys())
         res = []
-        for level in range(min_col, max_col + 1):
-            items = level_items[level]
-            items.sort(key = lambda x:(x[1], x[0]))
-            items = [val for val, _ in items]
-            res.append(items)
+
+        for col in sorted_columns:
+            column_nodes = sorted(column_items[col], key = lambda x:(x[0], x[1]))
+            res.append([val for row, val in column_nodes])
         
         return res
