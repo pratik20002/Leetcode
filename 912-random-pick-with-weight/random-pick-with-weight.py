ed48309c6587e1867a42292fd20b248c34239aa8
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-11-04 07:49:00 -0800

    Time: 80 ms (46.18%) | Memory: 21.9 MB (33.08%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -5,15 +5,18 @@ class Solution:
         for weight in w:
             total += weight
             self.prefix_sums.append(total)
-        
-    def pickIndex(self) -> int:
+   
+    #1
+    def pickIndex(self):
         target = random.randint(1, self.prefix_sums[-1])
         l, r = 0, len(self.prefix_sums) - 1
+
         while l < r:
-            mid = (l + r) // 2
+            mid = (l + r)//2
             if self.prefix_sums[mid] < target:
                 l = mid + 1
             else:
                 r = mid
             
         return l
+        
