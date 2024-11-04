baf6a3827a40b3e91bd193dd47bb21c522881ea9
Pratik Jadhav <55401192+pratik20002@users.noreply.github.com>
2024-11-03 16:18:42 -0800

    Time: 0 ms (100.00%) | Memory: 16.8 MB (8.57%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.py
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.py
@@ -1,29 +1,42 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
         
-        #CASE 1 - IF ARRAY DOES NOT START AT 1 
-        if arr[0] != 1:
-            if arr[0] - 1 >= k:
-                return k
-            else:
-                k -= arr[0] - 1
+        # #CASE 1 - IF ARRAY DOES NOT START AT 1 
+        # if arr[0] != 1:
+        #     if arr[0] - 1 >= k:
+        #         return k
+        #     else:
+        #         k -= arr[0] - 1
         
-        i = 0
-        #CASE 2 - IF THE K LIES IN BETWEEN
-        while i < len(arr) - 1:
-            diff = arr[i + 1] - arr[i]
-            if diff != 1:
-                for num in range(arr[i] + 1, arr[i + 1]):
-                    k -= 1
+        # i = 0
+        # #CASE 2 - IF THE K LIES IN BETWEEN
+        # while i < len(arr) - 1:
+        #     diff = arr[i + 1] - arr[i]
+        #     if diff != 1:
+        #         for num in range(arr[i] + 1, arr[i + 1]):
+        #             k -= 1
 
-                    if not k:
-                        return num
+        #             if not k:
+        #                 return num
             
-            i += 1
+        #     i += 1
         
-        #CASE 3 - IF K IS BEYOND THE ARR
-        if k:
-            return arr[-1] + k
+        # #CASE 3 - IF K IS BEYOND THE ARR
+        # if k:
+        #     return arr[-1] + k
+
+        left , right = 0, len(arr) - 1
+
+        while left <= right:
+            mid = (left + right)// 2
+            missing_count = arr[mid] - (mid + 1)
+
+            if missing_count < k:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return k + left
 
 
 # T -> O(N)
